807ef24730866351cce8fd843fddcfdb4fbd9e7c
rajkon <rkondala79@gmail.com>
2020-02-27 01:05:04 -0800

    mongdb atlas

--- server/server.js
+++ server/server.js
@@ -1,48 +1,116 @@
 const express = require('express')
 // const path = require('path');
-//const router = express.Router();
+const router = express.Router();
 const cors = require('cors')
-
-
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Data = require('./data');
+const mongoose = require('mongoose');
 // console.log(__dirname)
-const app = express()
+const server = express()
 
 // const pubDir = path.join(__dirname, '../public');
-
 // app.use(express.static(pubDir))
 
-app.use(cors());
-app.options('*', cors());
-app.get('/', (req, res) => {
+server.use(cors());
+server.options('*', cors());
+server.use(bodyParser.json());
+
+// << db setup >>
+const db = require("./db");
+const dbName = "datadb";
+const collectionName = "movies";
+server.get('/', (req, res) => {
     res.send("hello from api!");
 });
-app.set('port', process.env.PORT || 8080);
+
+// append /api for our http requests
+server.use('/api', router);
 
 
-app.get('/user', (req, res) => {
-    res.send({ "name": "user", "age": 0});
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(logger('dev'));
+
+
+// << db init >>
+db.initialize(dbName, collectionName, function (dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    // << db CRUD routes >>
+    server.post("/items", (request, response) => {
+        const item = request.body;
+        dbCollection.insertOne(item, (error, result) => { // callback of insertOne
+            if (error) throw error;
+            // return updated list
+            dbCollection.find().toArray((_error, _result) => { // callback of find
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+    server.get("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+
+        dbCollection.findOne({ id: itemId }, (error, result) => {
+            if (error) throw error;
+            // return item
+            response.json(result);
+        });
+    });
+
+    //Read all
+    server.get("/items", (request, response) => {
+        // return updated list
+        dbCollection.find().toArray((error, result) => {
+            if (error) throw error;
+            response.json(result);
+        });
+    });
+
+    //Update
+    server.put("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        const item = request.body;
+        console.log("Editing item: ", itemId, " to be ", item);
+
+        dbCollection.updateOne({ id: itemId }, { $set: item }, (error, result) => {
+            if (error) throw error;
+            // send back entire updated list, to make sure frontend data is up-to-date
+            dbCollection.find().toArray(function (_error, _result) {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+
+
+    server.delete("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        console.log("Delete item with id: ", itemId);
+
+        dbCollection.deleteOne({ id: itemId }, function (error, result) {
+            if (error) throw error;
+            // send back entire updated list after successful request
+            dbCollection.find().toArray(function (_error, _result) {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+}, function (err) { // failureCallback
+    throw (err);
 });
-
-
-// app.get('/help', (req, res) => {
-//     res.send(about.html)
-// })
-
-// app.get('/about', (req, res) => {
-//     res.send('About')
-// })
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'Please specify address'
-        })
-    }
-
-    res.send('Your weather')
-
-})
-
-app.listen(app.get('port'), () => {
-    console.log('Server is up on port 8080.')
-})
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+});
