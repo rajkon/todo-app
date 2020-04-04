552d109ee260e1d6236a408034c043f7367d6bb9
rajkon <rkondala79@gmail.com>
2020-04-03 18:01:49 -0700

    bootstrap to front end web app

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import "./index.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.css';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
